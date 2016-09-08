b33595e48bc4967ea574ef9fa04224dced13295e
DEAN, JOSHUA <735582@ehs-s-232-17.local>
2016-09-08 10:30:54 -0500

    Initial Commit

---
+++ SeatingChartIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeatingChartIOS
+//
+//  Created by DEAN, JOSHUA on 9/8/16.
+//  Copyright © 2016 DEAN, JOSHUA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
